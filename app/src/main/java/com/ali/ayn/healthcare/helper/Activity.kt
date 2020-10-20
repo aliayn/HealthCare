54e2e5b8fbf5485fe11f4652146451815ead449a
Ali Aynechian <aliaynechiyan@gmail.com>
2020-10-20 12:58:46 +0330

    remove unused class

--- app/src/main/java/com/ali/ayn/healthcare/helper/Activity.kt
+++ app/src/main/java/com/ali/ayn/healthcare/helper/Activity.kt
@@ -1,24 +1,9 @@
 package com.ali.ayn.healthcare.helper
 
-import android.os.Bundle
 import androidx.annotation.IdRes
-import androidx.annotation.Nullable
 import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.widget.Toolbar
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
 
-fun AppCompatActivity.initToolbar(toolbar: Toolbar, backEnabled: Boolean) {
-    setSupportActionBar(toolbar)
-    supportActionBar?.title = ""
-    supportActionBar?.setDisplayHomeAsUpEnabled(backEnabled)
-}
-
 fun AppCompatActivity.findNavController(@IdRes resId: Int): NavController =
     Navigation.findNavController(this, resId)
-
-fun AppCompatActivity.navigate(@IdRes resId: Int): Unit =
-    findNavController(resId).navigate(resId)
-
-fun AppCompatActivity.navigate(@IdRes resId: Int, @Nullable bundle: Bundle): Unit =
-    findNavController(resId).navigate(resId, bundle)
